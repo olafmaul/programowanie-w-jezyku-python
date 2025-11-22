6f578af797d77f6925f5f1e1d943f1f41b2d1ad7
Olaf Maul <olaf.maul@edu.uekat.pl>
2025-11-22 14:46:18 +0100

    zad_1.pl

--- zad_1.py
+++ zad_1.py
@@ -1,5 +1,4 @@
-#%%
-names = ['Martin', 'Tom', 'Eva']
-
+#Utwórz funkcję, która otrzyma w parametrze listę 5 imion, a następnie wyświetli każde z nich.
+names = ['Olaf', 'Tomasz', 'Andrzej', 'Marian', 'Slawomir']
 for name in names:
     print(name)
