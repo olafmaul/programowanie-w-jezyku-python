d13c2161e140e6cab6221b75a7198e8fab2e25a6
Olaf Maul <olaf.maul@edu.uekat.pl>
2025-12-13 12:21:43 +0100

    usuwanko

--- zad_11.py
+++
@@ -1,7 +0,0 @@
-def imie_nazwisko(name,surname):
-    return (f'Cześć{name}{surname}!')
-
-name = 'Olaf'
-surname = 'Maul'
-
-print(imie_nazwisko)
--- zad_22.py
+++
@@ -1,6 +0,0 @@
-def multiply(x, y):
-    return x * y
-
-multiply_both = multiply(2,3)
-
-print(multiply_both)
